bdb287896a024149b828b51de431596973b45121
Pratham2712 <prathamvaishya123@gmail.com>
2024-09-09 17:46:53 +0530

    full push

--- home/views.py
+++ home/views.py
@@ -21,6 +21,7 @@ import re
 from django.views.decorators.csrf import csrf_protect
 import numpy as np
 import os
+from core.settings import BASE_DIR
 
 
 truck_specs = {
@@ -140,7 +141,7 @@ def additionalInformationJson(request):
         user_exists = Users.objects.filter(email_id=email).exists()
         if user_exists:
             messages.error(request, "An account with this email already exists.")
-            return JsonResponse({"ERROR": "An account with this email already exists."}, status=400)
+            return JsonResponse({"ERROR": "An account with this email already exists."}, status=400)    
 
         # Create a new user
         user = Users(
@@ -710,9 +711,11 @@ def freeOutputJson(request):
         # print(threed_boxes)
         # print(container_inf)
     threed_data = []
-    base_dir = r'home\static\files'
+    # base_dir = r'home\static\files'
+    base_dir = BASE_DIR
     for path in threed_boxes:
         full_path = os.path.join(base_dir, os.path.basename(path))
+        print(full_path)
         try:
             with open(full_path, 'r') as file:
                 threed_data.append(json.load(file))
