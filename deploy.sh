0a7058a18a615ca5024d6ab40f4bd6c4ece9bdaa
EC2 Default User <ec2-user@ip-172-31-4-42.ap-south-1.compute.internal>
2024-10-01 06:14:06 +0000

    modified deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ pip install -r requirements.txt
 
 # Run database migrations
 echo "Running database migrations..."
-python manage.py migrate
+python3 manage.py migrate
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Restart Gunicorn
 echo "Restarting Gunicorn..."
