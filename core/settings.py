c0e455223530acef94237a8ef19db63a113e0c19
Pratham2712 <prathamvaishya123@gmail.com>
2024-11-06 17:27:14 +0530

    setting email change

--- core/settings.py
+++ core/settings.py
@@ -224,7 +224,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEFAULT_FROM_EMAIL = "Shahrukh@optipack3d.com"
+# DEFAULT_FROM_EMAIL = "Shahrukh@optipack3d.com"
+DEFAULT_FROM_EMAIL = "masterpalace12345@gmail.com"
 
 
 LOGIN_URL = 'login'
